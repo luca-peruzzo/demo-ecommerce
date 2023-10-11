4369e7424b6bb43a3328943b473e5a7cb62c1027
dbolzan <davide.bolzan@sharry.land>
2023-10-11 10:24:39 +0200

    up

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ApplicationRecord
     has_many :article_tags
     has_many :tags, through: :article_tags
+
+    scope :filter_by_title, lambda { |keyword| where('lower(title) LIKE ?', "%#{keyword.downcase}%")}
+
+    def self.search(params = {})
+      articles = Article.all
+
+      articles = articles.filter_by_title(params[:title]) if params[:title]
+
+      articles
+    end
 end
